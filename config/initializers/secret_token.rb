efcbcbea93bd76b4592e6680f8e080437dd5c627
David Eisinger <david.eisinger@gmail.com>
2010-12-12 17:19:05 -0500

    Initalize Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodFromTrucks::Application.config.secret_token = 'ee8b5128daa8a3ef2027e379e75ff65060b17aea6766ad7824f84a0913d2882f8605839bd0d2e3ab05a8651b238541d00f6b07560515f2828ecd667a5d50acfb'
